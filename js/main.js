8bd07cebb4e150c1a48428f3c5821bfe0783141e
Pius Wong <pius@pioslabs.com>
2016-11-25 17:22:41 -0600

    React to consecutive choices recursively

--- js/main.js
+++ js/main.js
@@ -10,9 +10,45 @@
     // constant mouse listener (keyboard option?)
   // ??history / high scores? / achievements? badges?
 
-var objWordChoices = wordsAll;
+// Set initial choices
+offerChoices(wordsAll);
 
-// Offer the initial choices
+var moreChoices = true;
+
+
+// Load up next choices (LOOP back) OR
+// go on to "end" state, passing final data in tree branch terminus
+
+// Display end state
+// show final sentence
+// display link to internet
+// calculate a score based on:
+// numHits (internet popularity)
+// year written or put on internet (age)
+// media type (video, social, blog, news, etc)
+// number of words in sentence
+// ?past tries
+// ?popularity of click compared to other players?
+// display witty comment on score? graphic?
+// Thanks for playing
+// display a SHARE button (Twitter, Facebook, Email)
+// emphasize the START OVER button
+// Use cookies to record score, sentences, history, etc???
+
+// ? History / high scores? / Achievements?
+// records of sentences/scores that are
+// youngest
+// oldest
+// oldest video
+// oldest social
+// oldest book
+// longest
+// shortest
+// going blue: has profanity or sex
+// mentions God, Lord, or Jesus
+
+// Offer word choices
+function offerChoices(objWordChoices){
   // Read in possible choices from word tree
   // Display choices randomly in the appropriate area
   //var choices = ["ChoiceA","ChoiceBisareallylongword","ChoiceC","ChoiceD"]; //DEBUG
@@ -35,66 +71,46 @@ var objWordChoices = wordsAll;
     // Reorder the choice according to random shuffle earlier
     $(".choice"+i).css("order",arrayOrder[i]);
   }
-    // animate?
+  // animate?
 
-// Respond to choice
+  // Respond to choice
   // Read in user choice
   for(let i=0; i<numChoices; i++){
     $(".choice"+i).click(function(){
-      //alert("The paragraph for choice"+i+" was clicked."); // DEBUG
       // Save word choice
       var chosenWord = choices[i];
       var chosenProperty = String.fromCharCode(i+65);
       // Display word choice in appropriate place
+      if(objWordChoices[chosenProperty].order>1){
+        chosenWord = " " + chosenWord;
+      }
       $(".messageDisplay").append(chosenWord);
       // Clear screen of old choices
       $(".mainWindow").empty();
-      // Find whether there are any more choices at least 3 layers down
-      // Does choice have "next" property?
-      console.log(objWordChoices[chosenProperty].hasOwnProperty("next"));
-        // If yes, is there more than 1 choice in the "next" property?
+      // Find whether there are any more choices deeper into the tree
+      // Does choice have "next" property? If yes...
+      if(objWordChoices[chosenProperty].hasOwnProperty("next")){
+        // ...is there more than 1 choice in the "next" property?
+        if(countElementsInObject(objWordChoices[chosenProperty].next)>1){
           // If there is more than 1 choice, then display choices like normal
-          // If there is only 1 choice, then does the next layer have more than 1 choice?
-            // If there is more than 1 choice, then display original choices like normal
-            // If there is only 1 choice, then does the next layer have more than 1 choice?
-              // If there is more than 1 choice, then display original choices like normal
-              // If there is only 1 choice, then assume there are no more choices.
-                // Go to the end state
+          // Go to initial loop
+          moreChoices = true;
+          // Read in the next set of choices for next cycle in while loop
+          offerChoices(objWordChoices[chosenProperty].next);
+        }
+        else if(countElementsInObject(objWordChoices[chosenProperty].next)===1){
+          // If there is only 1 choice, then ask the same questions to the next layer...
+          // recursive function to set moreChoices DEBUG
+        }
+      }
       // If no, then this is the last choice and last word. Go to "end" state
+      else{
+        // Exit initial loop
+        moreChoices = false;
+      }
     });
   }
-
-
-    // Load up next choices (LOOP back) OR
-    // go on to "end" state, passing final data in tree branch terminus
-
-// Display end state
-  // show final sentence
-  // display link to internet
-  // calculate a score based on:
-    // numHits (internet popularity)
-    // year written or put on internet (age)
-    // media type (video, social, blog, news, etc)
-    // number of words in sentence
-    // ?past tries
-    // ?popularity of click compared to other players?
-  // display witty comment on score? graphic?
-  // Thanks for playing
-  // display a SHARE button (Twitter, Facebook, Email)
-  // emphasize the START OVER button
-  // Use cookies to record score, sentences, history, etc???
-
-// ? History / high scores? / Achievements?
-  // records of sentences/scores that are
-    // youngest
-    // oldest
-    // oldest video
-    // oldest social
-    // oldest book
-    // longest
-    // shortest
-    // going blue: has profanity or sex
-    // mentions God, Lord, or Jesus
+}
 
 /** http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
  * Randomize array element order in-place.
